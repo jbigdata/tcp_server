de7105c2ac9c0d828a49fad54787052ec1b6c6a5
wan_liang@126.com <wan_liang@126.com>
2015-08-18 17:09:46 +0800

    commit

---
+++ src/main/java/com/wanliang/tcpserver/hanlder/TcpServerChannelInitializer.java
@@ -0,0 +1,43 @@
+package com.wanliang.tcpserver.hanlder;
+
+import com.wanliang.proto.chat.ClientServerMsg;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by piguangtao on 15/6/1.
+ */
+@Service
+public class TcpServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Autowired
+    @Qualifier("serverHandler")
+    private ChannelInboundHandler serverHandler;
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        //增加编解码
+        ch.pipeline()
+                .addLast(new LoggingHandler(LogLevel.INFO))
+//                .addLast("frameDecoder", new ProtobufVarint32FrameDecoder())
+                .addLast("frameDecoder", new LengthFieldBasedFrameDecoder(65536,0,4,0,4))
+                .addLast("pbDecoder", new ProtobufDecoder(ClientServerMsg.Req.getDefaultInstance()))
+//                .addLast("frameEncode", new ProtobufVarint32LengthFieldPrepender())
+                .addLast("frameEncode", new LengthFieldPrepender(4))
+                .addLast("protobufEncode", new ProtobufEncoder())
+                .addLast(new ReaderIdleHandler(60))
+                .addLast(serverHandler);
+    }
+}
